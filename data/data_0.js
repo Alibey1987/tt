5c8c9e0777a06a5fbdf1a852307a29e8cb41325d
Alibey1987 <140392328+Alibey1987@users.noreply.github.com>
2023-07-24 15:06:04 +0400

    Add files via upload

---
+++ data/data_0.js
@@ -0,0 +1,9 @@
+var json_data_0 = {
+"type": "FeatureCollection",
+"name": "data_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Info": null, "Rayonun ad": "Yasamal Rayonu", "Zona nömr": "5", "Əmsal": "1,10", "Növü": "Yeni tikili", "unvan": "Yasamal r-nu, Mirvari park", "sifarisci": "Əliyev Eldar Sahib o", "Qiymet": "858500 azn", "Agent": "Mikayıl Əliyev", "Sahe": "235" }, "geometry": { "type": "Point", "coordinates": [ 49.815344163024001, 40.383531774048933 ] } },
+{ "type": "Feature", "properties": { "id": "1", "Info": null, "Rayonun ad": "Yasamal Rayonu", "Zona nömr": "5", "Əmsal": "1,10", "Növü": "Yeni tikili", "unvan": "Yasamal r-nu, Qələbə dairəsi", "sifarisci": "Hüseynov Rahim İlham o", "Qiymet": "250000 azn", "Agent": "Elnur Azadov", "Sahe": "187" }, "geometry": { "type": "Point", "coordinates": [ 49.809734166741521, 40.393090406546648 ] } }
+]
+}
